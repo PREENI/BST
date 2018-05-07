807f4b47ad0340a694ac62eaffb64e3923f7eba1
PREENI <preeni.dhingra@gmail.com>
2018-05-07 10:11:45 +0530

    test (#2)
    * test
    
    * Vertical Sum
    
    * Update VerticalAddition.java
    
    * Update VerticalAddition.java

---
+++ src/chinks/VerticalAddition/VerticalAddition.java
@@ -0,0 +1,75 @@
+
+package chinks.VerticalAddition;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class VerticalAddition {
+   public class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        public Node (int a) {
+            this.left = null;
+            this.data = a;
+            this.right = null;
+        }
+   }
+   
+     Node root = null;
+     HashMap<Integer,Integer> map=new HashMap<>();
+     int a,flag;
+     
+   public void push(int a) {
+        root = insert(root,a);
+    }
+    
+    public Node insert(Node root,int a) {
+        if(root == null){
+            root = new Node(a);
+            return root;
+        }
+        else{
+            if(a < root.data) {
+                root.left = insert(root.left,a);
+            }
+            else if(a > root.data) {
+                root.right = insert(root.right,a);
+            }
+            return root;
+         }
+    }
+    
+    public void inorderrec() {
+        inorder(root,0);   
+        System.out.println(map);   
+    }
+    
+    
+    public void inorder(Node root,int R) {
+        if(root==null)
+            return ;
+                       
+        inorder(root.left,R-1);
+        if(map.get(R)==null) {
+            a=0;      
+        }
+        else
+            a=map.get(R);
+        map.put(R,a+root.data);
+        inorder(root.right,R+1);
+   } 
+       
+    public static void main(String args[]) {
+        VerticalAddition va=new VerticalAddition();
+        Scanner sc=new Scanner(System.in);
+        int a;
+        int n=sc.nextInt();
+        
+        for(int i=0;i<n;++i) {
+            a=sc.nextInt();
+            va.push(a);
+        }
+        va.inorderrec();
+    }
+ }
